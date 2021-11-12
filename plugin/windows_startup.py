913855b97551fd5967e7ade21d2ec606dafa0a1d
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-12 09:43:38 -0500

    init

---
+++ plugin/windows_startup.py
@@ -0,0 +1,12 @@
+from flox import Flox
+
+class WindowsStartup(Flox):
+
+    def query(self, query):
+        pass
+
+    def context_menu(self, data):
+        pass
+
+if __name__ == "__main__":
+    WindowsStartup()
