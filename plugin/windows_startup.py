02c46f0884b484885609fb0a31801615501ee10e
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-16 17:06:30 -0500

    Split action methods

--- plugin/windows_startup.py
+++ plugin/windows_startup.py
@@ -23,57 +23,67 @@ class WindowsStartup(Flox):
                     title=program.name.replace(".lnk", ""),
                     subtitle=subtitle,
                     icon=program.path,
-                    method=self.change_state,
-                    parameters=["toggle", str(program._base_key), str(program._reg_path), str(program._key_index)],
+                    method=self.toggle,
+                    parameters=[str(program._base_key), str(program._reg_path), str(program._key_index)],
                     context=[str(program._base_key), str(program._reg_path), str(program._key_index)]
                 )
 
     def context_menu(self, data):
         prog = startup.StartupProgram(int(data[0]), data[1], int(data[2]))
-        if prog.path == "None":
-            self.add_item(
-                title="Open Program",
-                subtitle=prog.path,
-                icon=prog.path,
-                method=self.open_program,
-                parameters=[prog.path]
-            )
-            self.add_item(
-                title="Reveal in Explorer",
-                subtitle=prog.path,
-                icon=str(Path(os.getenv('WINDIR'), 'explorer.exe')),
-                method=self.reveal_in_finder,
-                parameters=[prog.path]
-            )
+
+        self.add_item(
+            title="Open Program",
+            subtitle=prog.path,
+            icon=prog.path,
+            method=self.open_program,
+            parameters=[prog.path]
+        )
+        self.add_item(
+            title="Reveal in Explorer",
+            subtitle="Reveals and selects program in file explorer.",
+            icon=str(Path(os.getenv('WINDIR'), 'explorer.exe')),
+            method=self.reveal_in_finder,
+            parameters=[prog.path]
+        )
         self.add_item(
                 title="Enable Program",
                 subtitle="Allow program to start with Windows.",
                 icon=ENABLED_ICON,
-                method=self.change_state,
-                parameters=["enable", str(prog._base_key), str(prog._reg_path), str(prog._key_index)]
+                method=self.enable,
+                parameters=[str(prog._base_key), str(prog._reg_path), str(prog._key_index)]
         )
         self.add_item(
             title="Disable Program",
             subtitle="Prevent program from starting with Windows.",
             icon=DISABLED_ICON,
-            method=self.change_state,
-            parameters=["disable", str(prog._base_key), str(prog._reg_path), str(prog._key_index)]
+            method=self.disable,
+            parameters=[str(prog._base_key), str(prog._reg_path), str(prog._key_index)]
         )
 
-    def change_program(self, program: startup.StartupProgram, method: Callable, msg: str, fail_msg: str):
+    def change_program(self, program: startup.StartupProgram, method: Callable):
         _begin_state = program.value()
         method()
-        _format = {"name": program.name, "status": program.status.lower()}
+        title = self.manifest['Name']
+        icon = str(Path(self.plugindir).joinpath(self.icon))
         if program.value() != _begin_state:
-            msg = msg.format(**_format)
-            self.show_msg(f"{program.name} {program.status}", msg, ico_path=program.path)
+            title = f"{title}: {program.status}!"
+            msg = f"{program.name.title()} start-up is {program.status}"
         else:
-            fail_msg = fail_msg.format(**_format)
-            self.show_msg(f"Error", fail_msg.format(program), ico_path=ICON_APP_ERROR)
+            title = f"{title}: ERROR!"
+            msg = f"Unable to change {program.name} to {program.status}!"
+        self.show_msg(title, msg, ico_path=icon)
 
-    def change_state(self, state, base_key, path, key_index):
+    def enable(self, base_key, path, key_index):
         prog = startup.StartupProgram(int(base_key), path, int(key_index))
-        self.change_program(prog, getattr(prog, state), "Program start-up is {status}", "Unable to change {name} to {status}!")
+        self.change_program(prog, prog.enable)
+
+    def disable(self, base_key, path, key_index):
+        prog = startup.StartupProgram(int(base_key), path, int(key_index))
+        self.change_program(prog, prog.disable)
+
+    def toggle(self, base_key, path, key_index):
+        prog = startup.StartupProgram(int(base_key), path, int(key_index))
+        self.change_program(prog, prog.toggle)
 
     def open_program(self, path):
         os.startfile(path)
